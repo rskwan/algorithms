c57530bbc47988974166b5c77a9e4ea29c0ee1d7
Ronald Kwan <rkwan@berkeley.edu>
2012-09-28 00:49:10 -0700

    Made MersenneTwister a class to avoid using global

--- algorithms/generating/mersenne_twister.py
+++ algorithms/generating/mersenne_twister.py
@@ -11,43 +11,44 @@
     Time Complexity: 1
     Psuedocode: http://en.wikipedia.org/wiki/Mersenne_twister
 
-    mersenne_twister.extract_number() -> randnum
+    m = MersenneTwister()
+    m.extract_number() => randnum    
 
 """
 
-twister = []
-index = 0
+class MersenneTwister:
+    def __init__(self):
+        self.index = 0
+        self.twister = []
 
-def init_generator(seed, gen):
-    mask = pow(2, 32) - 1
-    for i in range(624):
-        if i == 0:
-            gen[i] = seed & mask
-        else:
-            gen[i] = (1812433253 * (gen[i - 1] ^ (gen[i - 1] >> 30)) + i)
-            gen[i] &= mask
+    def init_generator(self, seed, gen):
+        mask = pow(2, 32) - 1
+        for i in range(624):
+            if i == 0:
+                gen[i] = seed & mask
+            else:
+                gen[i] = (1812433253 * (gen[i - 1] ^ (gen[i - 1] >> 30)) + i)
+                gen[i] &= mask
 
-def extract_number():
-    global index
-    global twister
-    if twister == []:
-        twister = [None]*624
-        init_generator(19650218, twister)
-    if index == 0:
-        generate_numbers(twister)
-    y = twister[index]
-    y ^= y >> 11
-    y ^= (y << 7) & 2636928640
-    y ^= (y << 15) & 4022730752
-    y ^= y >> 18
-    index = (index + 1) % 624
-    return y
+    def extract_number(self):
+        if self.twister == []:
+            self.twister = [None]*624
+            self.init_generator(19650218, self.twister)
+        if self.index == 0:
+            self.generate_numbers(self.twister)
+        y = self.twister[self.index]
+        y ^= y >> 11
+        y ^= (y << 7) & 2636928640
+        y ^= (y << 15) & 4022730752
+        y ^= y >> 18
+        self.index = (self.index + 1) % 624
+        return y
 
-def generate_numbers(gen):
-    umask = pow(2, 31)
-    lmask = pow(2, 31) - 1
-    for i in range(624):
-        y = (gen[i] & umask) | (gen[(i + 1) % 624] & lmask)
-        gen[i] = gen[(i + 397) % 624] ^ (y >> 1)
-        if y % 2 != 0:
-            gen[i] ^= 2567483615
+    def generate_numbers(self, gen):
+        umask = pow(2, 31)
+        lmask = pow(2, 31) - 1
+        for i in range(624):
+            y = (gen[i] & umask) | (gen[(i + 1) % 624] & lmask)
+            gen[i] = gen[(i + 397) % 624] ^ (y >> 1)
+            if y % 2 != 0:
+                gen[i] ^= 2567483615
